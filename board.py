2d05000d366b41c9bdd108b18a1ec4a1c712776b
Eshaq-F <71772445+Eshaq-F@users.noreply.github.com>
2020-11-18 21:25:15 +0330

    Cleaning and refactoring.

--- board.py
+++ board.py
@@ -1,32 +1,26 @@
 import tkinter as tk
+from PIL import ImageTk
 from setting import *
 
 
-class LudoBoard:
+class MenschBoard:
 
     def __init__(self, master):
-        self.canvas = tk.Canvas(master, width=Board.BOARD_WIDTH, height=Board.BOARD_HEIGHT, bg=Color.DEFAULT,
+        self.canvas = tk.Canvas(master, width=Size.BOARD_WIDTH, height=Size.BOARD_HEIGHT, bg=Color.BG,
                                 highlightthickness=0)
-        self.side_bar = tk.Canvas(master, width=250, height=630, bg='white', highlightthickness=0)
-        self.label = tk.Label(self.side_bar, text='Players:', bg='white', font=("Times", "23", "bold italic"))
+        self.left_panel = tk.Canvas(master, width=250, height=630, bg=Color.WHITE, highlightthickness=0)
+        self.label = tk.Label(self.left_panel, text='Players:', bg=Color.WHITE, font=("Times", "23", "bold italic"))
+        self.img = ImageTk.PhotoImage(file='Images/black_hole.jpg')
+        self.black_hole = tk.Label(image=self.img, width=115, height=115)
         self.status_bar = tk.Label(master, text=Text.MADE_BY, bd=1, relief=tk.SUNKEN)
 
-    def draw_rectangle(self, lx, ly, bx, by, color, width):
+    def draw_rectangle(self, x, y, x1, y2, color, width):
         self.canvas.create_rectangle(
-            lx * Board.SQUARE_SIZE,
-            ly * Board.SQUARE_SIZE,
-            bx * Board.SQUARE_SIZE,
-            by * Board.SQUARE_SIZE,
-            fill=color,
-            width=width)
-
-    def draw_circle(self, x1, y1, x2, y2, color):
-        self.canvas.create_oval(
-            x1 * Board.SQUARE_SIZE,
-            y1 * Board.SQUARE_SIZE,
-            x2 * Board.SQUARE_SIZE,
-            y2 * Board.SQUARE_SIZE,
-            fill=color)
+            x * Size.PATH_SIZE,
+            y * Size.PATH_SIZE,
+            x1 * Size.PATH_SIZE,
+            y2 * Size.PATH_SIZE,
+            fill=color, width=width)
 
     def path(self):
 
@@ -35,8 +29,8 @@ class LudoBoard:
         for i in range(6, 9):
             for j in range(15):
                 if j not in range(6, 9) and (i != 7 or j == 0 or j == 14):
-                    self.draw_rectangle(i + 0.5, j + 0.5, i + 1.5, j + 1.5, Color.DEFAULT, 1)
-                    self.draw_rectangle(j + 0.5, i + 0.5, j + 1.5, i + 1.5, Color.DEFAULT, 1)
+                    self.draw_rectangle(i + 0.5, j + 0.5, i + 1.5, j + 1.5, Color.BG, 1)
+                    self.draw_rectangle(j + 0.5, i + 0.5, j + 1.5, i + 1.5, Color.BG, 1)
                 else:
                     if j < 6:
                         self.draw_rectangle(i + 0.5, j + 0.5, i + 1.5, j + 1.5, Color.YELLOW, 1)
@@ -45,31 +39,21 @@ class LudoBoard:
                         self.draw_rectangle(i + 0.5, j + 0.5, i + 1.5, j + 1.5, Color.RED, 1)
                         self.draw_rectangle(j + 0.5, i + 0.5, j + 1.5, i + 1.5, Color.BLUE, 1)
 
-        for i, j in Board.POSITIVE_V:
+        for i, j in Size.POSITIVE_V:
             if i > j:
                 self.draw_rectangle(i + 0.5, j + 0.5, i + 1.5, j + 1.5, Color.YELLOW, 1)
             else:
                 self.draw_rectangle(i + 0.5, j + 0.5, i + 1.5, j + 1.5, Color.RED, 1)
 
-        for j, i in Board.POSITIVE_H:
+        for j, i in Size.POSITIVE_H:
             if i > j:
                 self.draw_rectangle(j + 0.5, i + 0.5, j + 1.5, i + 1.5, Color.GREEN, 1)
             else:
                 self.draw_rectangle(j + 0.5, i + 0.5, j + 1.5, i + 1.5, Color.BLUE, 1)
 
-    # def start(self):
-    #     self.arrow_l = PhotoImage(file='Images/arrow_l.png')
-    #     self.arrow_r = PhotoImage(file='Images/arrow_r.png')
-    #     self.arrow_t = PhotoImage(file='Images/arrow_t.png')
-    #     self.arrow_b = PhotoImage(file='Images/arrow_b.png')
-    #     tk.Label(image=self.arrow_l, bg=Color.BLUE).place(x=915, y=343)
-    #     tk.Label(image=self.arrow_t, bg=Color.RED).place(x=635, y=543)
-    #     tk.Label(image=self.arrow_r, bg=Color.GREEN).place(x=435, y=263)
-    #     tk.Label(image=self.arrow_b, bg=Color.YELLOW).place(x=715, y=63)
-
     def home(self):
 
-        for i, j in Board.POINTS:
+        for i, j in Size.POINTS:
 
             if i == 0 and j == 0:
                 self.draw_rectangle(i * 9 + 0.5, j * 9 + 0.5, i * 9 + 6.5, j * 9 + 6.5, Color.GREEN, 3)
@@ -80,9 +64,9 @@ class LudoBoard:
             else:
                 self.draw_rectangle(i * 9 + 0.5, j * 9 + 0.5, i * 9 + 6.5, j * 9 + 6.5, Color.BLUE, 3)
 
-            self.draw_rectangle(i * 9 + 1.25, j * 9 + 1.25, i * 9 + 5.75, j * 9 + 5.75, 'white', 0)
+            self.draw_rectangle(i * 9 + 1.25, j * 9 + 1.25, i * 9 + 5.75, j * 9 + 5.75, Color.WHITE, 0)
 
-        for i, j in Board.POINTS:
+        for i, j in Size.POINTS:
 
             if i == 0 and j == 0:
                 self.draw_rectangle(i * 9 + 1.65, j * 9 + 1.65, i * 9 + 3.3, j * 9 + 3.3, Color.GREEN, 0)
@@ -105,21 +89,21 @@ class LudoBoard:
                 self.draw_rectangle(i * 9 + 1.65, j * 9 + 3.65, i * 9 + 3.3, j * 9 + 5.3, Color.BLUE, 0)
                 self.draw_rectangle(i * 9 + 3.65, j * 9 + 1.65, i * 9 + 5.3, j * 9 + 3.3, Color.BLUE, 0)
 
-    def create_panel(self):
-        self.side_bar.place(x=0, y=0)
+    def draw_sidebar(self):
+        self.left_panel.place(x=0, y=0)
         self.label.place(x=65, y=0)
-        self.side_bar.create_line(249, 0, 249, 630)
-        self.side_bar.create_line(3, 280, 246, 280, dash=(4, 2))
+        self.black_hole.place(x=631, y=261)
+        self.left_panel.create_line(249, 0, 249, 630)
+        self.left_panel.create_line(3, 280, 246, 280, dash=(4, 2))
         self.status_bar.pack(side=tk.BOTTOM, fill=tk.X)
 
     def create(self):
         self.path()
         self.home()
-        # self.start()
-        self.create_panel()
+        self.draw_sidebar()
 
     def get_canvas(self):
         return self.canvas
 
     def get_frame(self):
-        return self.side_bar
+        return self.left_panel
